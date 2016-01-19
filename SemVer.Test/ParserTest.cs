781d73b90df76cefb395869d707738008a704b34
Joe <zhuzuxin@Live.com>
2016-01-20 01:27:13 +0800

    add unit test for parse

--- SemVer.Test/ParserTest.cs
+++ SemVer.Test/ParserTest.cs
@@ -5,10 +5,24 @@ namespace SemVer.Test
     public class ParserTest
     {
         [FactAttribute]
-        public void GeneralParse()
+        public void GeneralParseTest()
         {
             SemanticVersion v;
             Assert.True(SemanticVersion.TryParse("0.2.1", out v));
+        }
+
+        [FactAttribute]
+        public void EmptyStrTest()
+        {
+            SemanticVersion v;
+            Assert.False(SemanticVersion.TryParse(string.Empty, out v));
+        }
+
+        [FactAttribute]
+        public void ShortStrTest()
+        {
+            SemanticVersion v;
+            Assert.False(SemanticVersion.TryParse("0.1.", out v));
         }
     }
 }
